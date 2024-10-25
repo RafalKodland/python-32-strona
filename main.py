3eb2ad5ae72e35c99f089552fcc9b54fb394b233
Rafał Rusin <rafal.rusin@o365.us.edu.pl>
2024-10-25 18:01:06 +0200

    Kod startowy

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-import flask
+from flask import Flask
+from random import randint
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Witaj świecie! Test</p>"
+
+@app.route("/test")
+def test():
+    return f"<p>To jest strona testowa. Liczba testowa: {randint(1, 10)}</p>"
+
+app.run(debug=True)
